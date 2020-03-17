8c14131c052cc1e000e304a12d0be2a21e29c397
KarpusKonstantin <60568968+KarpusKonstantin@users.noreply.github.com>
2020-03-17 09:34:51 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    function getBracketsArray (config) {
+       // console.log(config);
+        return config.map(function(item) {
+         //   console.log(item.join(''));
+            return item.join('');
+        });
+    }
+
+    let bracketsArray = getBracketsArray(bracketsConfig);
+
+   // console.log(str, bracketsArray);
+
+    for (let i = 0; i < bracketsArray.length;) {
+        if (str.indexOf(bracketsArray[i]) !== -1) {
+            str = str.replace(bracketsArray[i], '');
+      //      console.log(str);
+            i = 0;
+        } else i++;
+    };
+
+    return !str;
+
 }
